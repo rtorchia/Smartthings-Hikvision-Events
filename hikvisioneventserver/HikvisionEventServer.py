0dee840cd261e8ebef89aac19511d8c9952c424f
rtorchia <ralphtorchia1@gmail.com>
2020-04-20 20:04:26 -0400

    Updated for Python 3.8
    Code updated to be compatible with Python 3. Also made some other updates.

--- hikvisioneventserver/HikvisionEventServer.py
+++ hikvisioneventserver/HikvisionEventServer.py
@@ -1,13 +1,18 @@
-## python2.7
+#!/usr/bin/python3.8
+## python3.8
 ## Hikvision Event Server
 ## Tested with Hikvison NVR DS-7608NI-EV2 / 8P
 ## Written by Ralph Torchia (ralphtorchia1 at gmail dot com)
-## Copyrighted 2019
+## Copyrighted 2019-2020
 ## This code is under the terms of the GPL v3 license.
 
-import ConfigParser
+## v1.2
+
+import sys
+import configparser
 import json
 import requests
+import os
 
 from hikvisionapi import Client
 
@@ -15,7 +20,7 @@ from hikvisionapi import Client
 class HVServerConfig():
 	def __init__(self, configfile):
 
-		self._config = ConfigParser.ConfigParser()
+		self._config = configparser.ConfigParser()
 		self._config.read(configfile)
 		self.CAMERA = ['0', '1', '2', '3', '4', '5', '6', '7']
 
@@ -37,23 +42,36 @@ class HVServerConfig():
 					return self._config.getboolean(section,variable)
 				elif type == 'int':
 					return int(self._config.get(section,variable))
-			except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-				self.defaulting(section, variable, default, quiet)
+			except (configparser.NoSectionError, configparser.NoOptionError):
+#				self.defaulting(section, variable, default, quiet)
 				return default
 
 # Main
 while True:
-	cfgfile='hikvisionevents.cfg'
-	config = HVServerConfig(cfgfile)
-
-	nvr = Client(config.IPADDR, config.USER, config.PASS)
-	print 'Monitoring for events from NVR @ http://'+config.IPADDR
-	nvrinfo = nvr.System.deviceInfo(method='get')
-	print '  Name: ' + nvrinfo['DeviceInfo']['deviceName']
-	print '  Serial: ' + nvrinfo['DeviceInfo']['serialNumber']
-	print '  Model: ' + nvrinfo['DeviceInfo']['model']
-	print '  Firmware: ' + nvrinfo['DeviceInfo']['firmwareVersion']
+	cfgfile = 'hikvisionevents.cfg'
 	
+	#cfgfile=cfgpath + 'hikvisionevents.cfg'
+	pathname = os.path.dirname(sys.argv[0])
+	cfgpath = os.path.abspath(pathname)
+	configfile = os.path.join(cfgpath, cfgfile)
+		
+	if os.path.exists(configfile):
+		print (f'Hikvisoin Events Server loading {cfgfile} file.')
+		config = HVServerConfig(configfile)
+		print ('Connecting to Hikvision NVR...')
+		nvr = Client(config.IPADDR, config.USER, config.PASS)
+		print('Monitoring for events from NVR @ http://'+config.IPADDR)
+		nvrinfo = nvr.System.deviceInfo(method='get')
+		print('  Name: ' + nvrinfo['DeviceInfo']['deviceName'])
+		print('  Serial: ' + nvrinfo['DeviceInfo']['serialNumber'])
+		print('  Model: ' + nvrinfo['DeviceInfo']['model'])
+		print('  Firmware: ' + nvrinfo['DeviceInfo']['firmwareVersion'])
+		print('  Cameras: ') 
+		for i in range (8):
+			print('     ' + str(i+1) + ') ' + config.CAMERA[i])
+	else:
+		sys.exit('ERROR: hikvisionevents.cfg file not found!')
+		
 	while True:
 		try:
 
@@ -66,7 +84,7 @@ while True:
 					eventchannelID = 'channelID'
 	
 				cameranumber = int(nvrevent[0]['EventNotificationAlert'][eventchannelID]) - 1
-				if  cameranumber not in range (0,8) :
+				if  cameranumber not in list(range(0,8)) :
 					cameranumber = 0
 			
 				body = {
@@ -85,18 +103,16 @@ while True:
 						}
 
 				urlreq = "https://" + urls['host'] + ":" + urls['port'] + urls['path']
-				print " Event detected: " + body['eventType'] + " occurred at " + body['eventTime'] + " for camera " + body['cameraName'] + " (channel=" + body['channelName'] + ")"
+				print(" Event detected: " + body['eventType'] + " occurred at " + body['eventTime'] + " for camera " + body['cameraName'] + " (channel=" + body['channelName'] + ")")
 
 				stconn = requests.put(urlreq, data=json.dumps(body), headers=headers)
 			else:
-				print 'Could not open stream to NVR!'
-				exit()
+				sys.exit('ERROR: Could not open stream to NVR!')				
 	
 		except IOError:
 			pass
 		except KeyboardInterrupt:
-			print 'Program exececution halted by user'
-			exit()
+			sys.exit('Program exececution halted by user')
 		except Exception as exception:
-			print '--> ', exception
-#			pass
+			print('--> ', exception)
+			#pass
